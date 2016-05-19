85085838942d564722800c6f1bd38e787116d77e
Alex Ramirez <r.alexramirez@outlook.com>
2016-05-19 12:51:12 -0400

    bloccit checkpoint 9 assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,17 @@ posts = Post.all
   )
 end
 
+10.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+  )
+ end
+ sponsored_posts = SponsoredPost.all
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored posts created"
---
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe SponsoredPostsController, type: :controller do
+
+  let(:my_topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
+  let(:my_sponsored_post) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, topic_id: my_topic.id, id: my_sponsored_post.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new, topic_id: my_topic.id, id: my_sponsored_post.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, topic_id: my_topic.id, id: my_sponsored_post.id
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
