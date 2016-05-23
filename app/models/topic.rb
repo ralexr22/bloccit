f13dab426d9b8926d6b55373084ed00146acea1d
Alex Ramirez <r.alexramirez@outlook.com>
2016-05-22 21:04:04 -0400

    bloccit checkpoint 10 assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,4 @@ class Topic < ActiveRecord::Base
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
-  validates :topic, presence: true
 end
