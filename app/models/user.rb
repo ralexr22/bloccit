f6b494bc7aa6a016814b073528789231cc6f6aa3
Alex Ramirez <r.alexramirez@outlook.com>
2016-05-25 22:33:48 -0400

    bloccit checkpoint 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
-  before_save { self.name = name.capitalize_names if name.present? }
+  #before_save { self.name = name.capitalize_names if name.present? }
 
-  def capitalize_names
-    name.split.each do |user|
-      user.capitalize
-    end
+  #def capitalize_names
+  #  name.split.each do |user|
+  #    user.capitalize
+  #  end
 
-    name = capitalize_names.join(" ")
-  end
+  #  name = capitalize_names.join(" ")
+  #end
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
