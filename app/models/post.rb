00a802568beb6b1e779a813e97b561ab2895d081
Alex Ramirez <r.alexramirez@outlook.com>
2016-06-25 23:30:18 -0400

    bloccit checkpoint 43

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -29,7 +29,7 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
-  end 
+  end
 
   scope :ordered_by_title, -> { order 'title DESC' }
   scope :ordered_by_reverse_created_at, -> { order 'created ASC' }
