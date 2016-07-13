ae803ffdc2b5c60fca12ff6ad9e37356ed03c01b
Alex Ramirez <r.alexramirez@outlook.com>
2016-07-13 09:57:00 -0400

    Checkpoint 47

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -11,71 +11,72 @@ RSpec.describe CommentsController, type: :controller do
   context "guest" do
      describe "POST create" do
        it "redirects the user to the sign in view" do
-         post :create, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
+         post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
          expect(response).to redirect_to(new_session_path)
        end
      end
 
      describe "DELETE destroy" do
        it "redirects the user to the sign in view" do
-         delete :destroy, post_id: my_post.id, id: my_comment.id
+         delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
          expect(response).to redirect_to(new_session_path)
        end
      end
-   end
-   context "member user doing CRUD on a comment they don't own" do
-    before do
-      create_session(other_user)
-    end
 
-    describe "POST create" do
-      it "increases the number of comments by 1" do
-        expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
+     context "member user doing CRUD on a comment they don't own" do
+       before do
+         create_session(other_user)
+       end
+
+        describe "POST create" do
+          it "increases the number of comments by 1" do
+            expect{ post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
+          end
+
+          it "returns http success" do
+            post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
+            expect(response).to have_http_status(:success)
+          end
+        end
+
+        describe "DELETE destroy" do
+          it "redirects the user to the posts show view" do
+            delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+            expect(response).to redirect_to([my_topic, my_post])
+          end
+        end
+       end
+
+       context "member user doing CRUD on a comment they own" do
+        before do
+          create_session(my_user)
+        end
+
+        describe "POST create" do
+          it "increases the number of comments by 1" do
+            expect{ post :create,format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
+          end
+
+          it "returns http success" do
+            post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
+            expect(response).to have_http_status(:success)
+          end
+        end
+
+        describe "DELETE destroy" do
+          it "deletes the comment" do
+            delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+            count = Comment.where({id: my_comment.id}).count
+            expect(count).to eq 0
+          end
+
+          it "returns http success" do
+            delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+            expect(response).to have_http_status(:success)
+          end
+        end
       end
 
-      it "redirects to the post show view" do
-        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-        expect(response).to redirect_to [my_topic, my_post]
-      end
-    end
-
-    describe "DELETE destroy" do
-      it "redirects the user to the posts show view" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
-        expect(response).to redirect_to([my_topic, my_post])
-      end
-    end
-  end
-
-  context "member user doing CRUD on a comment they own" do
-    before do
-      create_session(my_user)
-    end
-
-    describe "POST create" do
-      it "increases the number of comments by 1" do
-        expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
-      end
-
-      it "redirects to the post show view" do
-        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-        expect(response).to redirect_to [my_topic, my_post]
-      end
-    end
-
-    describe "DELETE destroy" do
-      it "deletes the comment" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
-        count = Comment.where({id: my_comment.id}).count
-        expect(count).to eq 0
-      end
-
-      it "redirects to the post show view" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
-        expect(response).to redirect_to [my_topic, my_post]
-      end
-    end
-  end
   context "admin user doing CRUD on a comment they don't own" do
     before do
       other_user.admin!
@@ -84,26 +85,27 @@ RSpec.describe CommentsController, type: :controller do
 
     describe "POST create" do
       it "increases the number of comments by 1" do
-        expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
+        expect{ post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
       end
 
-      it "redirects to the post show view" do
-        post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-        expect(response).to redirect_to [my_topic, my_post]
+      it "returns http success" do
+        post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+        expect(response).to have_http_status(:success)
       end
     end
 
     describe "DELETE destroy" do
       it "deletes the comment" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
         count = Comment.where({id: my_comment.id}).count
         expect(count).to eq 0
       end
 
-      it "redirects to the post show view" do
-        delete :destroy, post_id: my_post.id, id: my_comment.id
-        expect(response).to redirect_to [my_topic, my_post]
+      it "returns http success" do
+        delete :destroy, format: :js, post_id: my_post.id, id: my_comment.id
+        expect(response).to have_http_status(:success)
       end
     end
+   end
   end
 end
