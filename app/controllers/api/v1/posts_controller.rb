bc377d12fd64382c39ba5321cdeb1a04891d3fb6
Alex Ramirez <r.alexramirez@outlook.com>
2016-08-12 23:19:22 -0400

    fixed checkpoint 49

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,9 @@
 class API::V1::PostsController < Api::V1::TopicsController
 
   def create
+    topic = Topic.find(params[:topic_id])
     post = Post.new(post_params)
+    post.topic = topic
 
     if post.valid?
       post.save!
